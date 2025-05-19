dd0aeb77cc4f36ebade7275a87dfbf1702ba02cc
maricel <maricelmb@gmail.com>
2025-05-19 14:18:25 +1000

    s3.23: Display a list of products

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+const products = [
+  {name: 'product1', price: 100.0},
+  {name: 'product2', price: 100.0},
+  {name: 'product2', price: 100.0}
+]
 
 function App() {
-  return <div style={{fontSize: '1.8rem'}}>Ski Store</div>;
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>Ski Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        )
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
