ec37d3470af73b47270241ae247549ed6654e5d0
maricel <maricelmb@gmail.com>
2025-05-23 21:01:14 +1000

    s3.28: Adding a product type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
+import type { Product } from "./product";
 
 
 
 function App() {
-  const [products, setProducts] = useState<{name: string, price: number}[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch('https://localhost:7269/api/products')
@@ -13,8 +14,16 @@ function App() {
 
   const addProduct = () =>{
     setProducts(prevState => [...prevState, 
-        {name: 'product' + (prevState.length + 1), 
-          price: (prevState.length * 100) + 100}])
+      {
+        id: prevState.length + 1,
+        name: 'product' + (prevState.length + 1), 
+        price: (prevState.length * 100) + 100,
+        quantityInStock: 100,
+        description: 'test',
+        pictureUrl: 'https://picsum.photo/200',
+        type: 'test',
+        brand: 'test'
+      }])
   }
 
   return (
