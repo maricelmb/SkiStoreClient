7204b6670dd12845f10780cb5dfe44591efd0d69
maricel <maricelmb@gmail.com>
2025-05-27 22:46:53 +1000

    S3.29: Restructure folders

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import type { Product } from "../models/product";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7269/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const addProduct = () => {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: "product" + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+        quantityInStock: 100,
+        description: "test",
+        pictureUrl: "https://picsum.photo/200",
+        type: "test",
+        brand: "test",
+      },
+    ]);
+  };
+
+  return (
+    <div>
+      <h1 style={{ color: "red" }}>Ski Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </div>
+  );
+}
+
+export default App;
