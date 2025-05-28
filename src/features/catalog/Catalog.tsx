d97b06ce4d41f3bf57084b5ee7e95b02f8ee04ec
maricel <maricelmb@gmail.com>
2025-05-28 22:08:32 +1000

    s3.32 Using MUI components

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,21 +1,16 @@
+
 import type { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 
 type Props = {
   products: Product[];
-  addProduct: () => void;
+  
 }
 
-export default function Catalog({products, addProduct}: Props) {
+export default function Catalog({products}: Props) {
   return (
     <>
-       <ul>
-        {products.map(item  => (
-          <li key={item.id}>
-            {item.name} - {item.price}
-          </li>
-        ))}        
-      </ul>
-      <button onClick={addProduct} >Add Product</button>
+      <ProductList products={products} />
     </>
   )
 }
---
+++ src/features/catalog/ProductList.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+import ProductCard from "./ProductCard"
+import type { Product } from "../../app/models/product"
+
+type Props = {
+    products: Product[]
+}
+
+export default function ProductList({products}: Props) {
+  return (
+    <Box sx={{display: 'flex', flexWrap: 'wrap', gap:3, justifyContent: 'center'}}>
+        {products.map(product  => (
+          <ProductCard product={product}/>
+        ))}        
+    </Box>
+  )
+}
