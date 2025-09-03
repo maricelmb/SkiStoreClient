dd62ef2f90bbf3124e17bc09a19fa8e031759fa2
maricel <maricelmb@gmail.com>
2025-09-03 22:50:05 +1000

    Displaying content of product details

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import type { Product } from "../../app/models/product";
+import {
+  Button,
+  Divider,
+  Grid2,
+  Table,
+  TableBody,
+  TableContainer,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -13,5 +23,45 @@ export default function ProductDetails() {
       .catch((error) => console.log(error));
   }, [id]);
 
-  return <div>{product?.name}</div>;
+  if (!product) return <div>Loading...</div>;
+
+  return (
+    <Grid2 container spacing={6} maxWidth="lg" sx={{ mx: "auto" }}>
+      <Grid2 size={6}>
+        <img
+          src={product?.pictureUrl}
+          alt={product.name}
+          style={{ width: "100%" }}
+        />
+      </Grid2>
+      <Grid2 size={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>Table goes here</TableBody>
+          </Table>
+        </TableContainer>
+        <Grid2 container spacing={2} marginTop={3}>
+          <Grid2 size={6}>
+            <TextField
+              variant="outlined"
+              type="number"
+              label="Quantity in basket"
+              fullWidth
+              defaultValue={1}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <Button color="primary" size="large" variant="contained" fullWidth>
+              Add to Basket
+            </Button>
+          </Grid2>
+        </Grid2>
+      </Grid2>
+    </Grid2>
+  );
 }
