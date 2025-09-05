81b885f3b9ff2eb565441a53409ec1c3c3c28f40
maricel <maricelmb@gmail.com>
2025-09-05 15:31:28 +1000

    redux actions

--- src/features/contact/ContactPage.tsx
+++ src/features/contact/ContactPage.tsx
@@ -1,14 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import type { CounterState } from "./counterReducer";
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
 
 export default function ContactPage() {
   const data = useSelector((state: CounterState) => state.data);
+  const dispatch = useDispatch();
 
   return (
     <>
       <Typography variant="h2">Contact page</Typography>
       <Typography variant="body1">The data is : {data}</Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch({ type: "decrement" })} color="error">
+          Decrement
+        </Button>
+        <Button
+          onClick={() => dispatch({ type: "increment" })}
+          color="secondary"
+        >
+          Increment
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
