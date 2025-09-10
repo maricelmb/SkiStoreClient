c2c5ba6ed079b062772220e4e8946a69006c2a48
maricel <maricelmb@gmail.com>
2025-09-10 19:20:56 +1000

    save dark mode state to local storage

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 import {
   Box,
   Container,
@@ -11,9 +10,13 @@ import {
 import NavBar from "./NavBar";
 import { Outlet } from "react-router";
 
+const getInitialDarkMode = () => {
+  const storedDarkMode = localStorage.getItem("darkMode");
+  return storedDarkMode ? JSON.parse(storedDarkMode) : true;
+};
+
 function App() {
-  
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(getInitialDarkMode);
   const paletteType = darkMode ? "dark" : "light";
 
   const theme = createTheme({
@@ -26,26 +29,25 @@ function App() {
   });
 
   const toggleDarkMode = () => {
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
     setDarkMode(!darkMode);
-  }
-
- 
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+      <NavBar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
       <Box
         sx={{
           minHeight: "100vh",
-          background: darkMode 
-          ? "radial-gradient(circle, #1e3aBa, #111B27" 
-          : "radial-gradient(circle, #baecf9, #f0f9ff",
-          py: 6
+          background: darkMode
+            ? "radial-gradient(circle, #1e3aBa, #111B27"
+            : "radial-gradient(circle, #baecf9, #f0f9ff",
+          py: 6,
         }}
       >
-        <Container maxWidth="xl" sx={{ mt: 14 }}>    
-          <Outlet />     
+        <Container maxWidth="xl" sx={{ mt: 14 }}>
+          <Outlet />
         </Container>
       </Box>
     </ThemeProvider>
