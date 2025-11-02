dbb0e77738b9b342042e6aa633bdfe3162bcfa60
maricel <maricelmb@gmail.com>
2025-11-02 23:13:20 +1100

    auto update cart page after adding an item to cache with refreshing the page manually

--- src/features/basket/basketApi.ts
+++ src/features/basket/basketApi.ts
@@ -5,9 +5,12 @@ import type { Basket } from "../../app/models/basket";
 export const basketApi = createApi({
   reducerPath: "basketApi",
   baseQuery: baseQueryWithErrorHandling,
+  tagTypes: ["Basket"], //define tag category
   endpoints: (builder) => ({
+    //get basket and tag it
     fetchBasket: builder.query<Basket, void>({
       query: () => "basket",
+      providesTags: ["Basket"],
     }),
     addBasketItem: builder.mutation<
       Basket,
@@ -17,6 +20,15 @@ export const basketApi = createApi({
         url: `basket?productId=${productId}&quantity=${quantity}`,
         method: "POST",
       }),
+      onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
+        try {
+          await queryFulfilled;
+          //if post is successful, the cached data is already stale so RTK query needs to refetch
+          dispatch(basketApi.util.invalidateTags(["Basket"]));
+        } catch (error) {
+          console.log(error);
+        }
+      },
     }),
     removeBasketItem: builder.mutation<
       void,
